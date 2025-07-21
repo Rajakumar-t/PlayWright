8e3e93b83b68b739c3831965168a146a4268921e
Rajakumar-t <rajakumart@gmail.com>
2025-07-21 11:12:54 +0100

    Update 5-gradeCalculation.js

--- tests/HomeWork/week1/5-gradeCalculation.js
+++ tests/HomeWork/week1/5-gradeCalculation.js
@@ -1,8 +1,10 @@
 const { log } = require("console");
 const { loadavg } = require("os");
-
+//creating a fuction
 function studentScore(marks) {
+    //creating a variable to store the result
     let grade;
+    //creating a swith condition to match if hte case is true
     switch (true) {
         case marks >= 70:
             console.log("Student Grade = First Class");
@@ -19,6 +21,8 @@ function studentScore(marks) {
         default:
             grade = "Invalid Score";
     }
+    //returning the resulut that stored in grade variable
     return grade
 }
+//function is called 
 studentScore(10)
